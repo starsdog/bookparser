82bce4ac8c033ed170c4a8764e4c036562c803a5
ling.chen <ling.chen@athentek.com>
2017-08-03 05:36:26 +0000

    add image download function

--- parser.py
+++ parser.py
@@ -9,12 +9,14 @@ import csv
 
 class htmlParser(object):
     def __init__(self, config_file):
+        self.header={"Connection":"close"}
         with open(config_file) as file:
             config = json.load(file)
             for key in config.keys():
                 setattr(self, key, config[key])
     
     def parse_html(self, filename):
+        print(filename)
         parser=etree.HTMLParser()
         try:
             tree=html.parse(filename)
@@ -44,45 +46,48 @@ class htmlParser(object):
                 content['genre']=[]
                 for info in tree.xpath('//li//span/following-sibling::span/a'):
                     if info.attrib.get('class')=='linkStyle02': 
-                        print("info={}, {}".format(info.getparent().tag, info.text))
+                        #print("info={}, {}".format(info.getparent().tag, info.text))
                         content['genre'].append(info.text)
         
         tag_info=tree.xpath("//a[@class='tag']")
         content['tag']=[]
         for p in tag_info:
-            print("p={}".format(p.text))
+            #print("p={}".format(p.text))
             content['tag'].append(p.text)
 
         brief_info=tree.xpath("//div[@id='prodPfDiv']")
         desc=''
         ad_word=['關鍵特色', '好評推薦', '作者簡介', '佳評如潮', '暢銷書']
-        for child in brief_info[0]:
-            if child.text != None and '作者簡介' in child.text:
-                break
+        if len(brief_info):
+            for child in brief_info[0]:
+                if child.text != None and '作者簡介' in child.text:
+                    break
 
-            cleaner = Cleaner()
-            cleaner.remove_tags = ['p','br','span','font','b','center']
-            innertext=etree.tostring(child, encoding='unicode', method='html').replace("<div>","").replace("</div>","")
+                cleaner = Cleaner()
+                cleaner.remove_tags = ['p','br','span','font','b','center']
+                innertext=etree.tostring(child, encoding='unicode', method='html').replace("<div>","").replace("</div>","")
 
-            cleaned=cleaner.clean_html(innertext).replace("<div>","").replace("</div>","")
+                cleaned=cleaner.clean_html(innertext).replace("<div>","").replace("</div>","")
             
-            ad_exist=False
-            for word in ad_word:
-                if word in cleaned:
-                    ad_exist=True
+                ad_exist=False
+                for word in ad_word:
+                    if word in cleaned:
+                        ad_exist=True
 
-            if ad_exist==True:
-                break
+                if ad_exist==True:
+                    break
 
-            desc += cleaned  
+                desc += cleaned  
         content['desc']=desc
 
         head, tail=os.path.split(filename)
         tazze_link='https://www.taaze.tw/sing.html?pid='+tail[:-5]
         content['link']={'tazze':tazze_link}
         
-        print("content={}".format(content))
+        #print("content={}".format(content))
 
+        if 'ISBN_no' not in content.keys():
+            content['ISBN_no']=tail[:-5]
         filename="{}.json".format(content['ISBN_no'])
         file_path=os.path.join(self.json_folder, filename)
         output=open(file_path, "w")
@@ -93,8 +98,7 @@ class htmlParser(object):
 
     def download_html(self, pid):
         url='https://www.taaze.tw/sing.html?pid='+str(pid)
-        header={"Connection":"close"}
-        r=requests.get(url, headers=header)
+        r=requests.get(url, headers=self.header)
         if r.status_code==200:
             filename='{}.html'.format(pid)
             output_file=os.path.join(self.html_folder, filename)
@@ -104,6 +108,30 @@ class htmlParser(object):
             return True
         else: 
             return False
+    
+    def download_img(self, pid, isbn):
+        large=False
+        small=False
+
+        url='https://media.taaze.tw/showLargeImage.html?sc='+str(pid)
+        r=requests.get(url, headers=self.header)
+        if r.status_code==200:
+            filename='{}.jpg'.format(isbn)
+            output_file=os.path.join(self.large_media_folder, filename)
+            output=open(output_file, "wb")
+            output.write(r.content)
+            output.close()
+            large=True
+            url='https://media.taaze.tw/showProdImage.html?sc='+str(pid)
+            r=requests.get(url, headers=self.header)
+            if r.status_code==200:
+                output_file=os.path.join(self.small_media_folder, filename)
+                output=open(output_file, "wb")
+                output.write(r.content)
+                output.close()
+                small=True
+            
+        return large, small
 
 if __name__=="__main__":
     arg_parser = argparse.ArgumentParser()
@@ -141,5 +169,14 @@ if __name__=="__main__":
             status=parser.download_html(pid)
             writer.writerow({"pid":pid, "status":status})
         status_output.close()
+    elif task=='image':
+        status_output=open(project_config['image_status_csv'], 'a')
+        isbn_input=open(project_config['parse_status_csv'], 'r')
+        reader=csv.DictReader(isbn_input)
+        writer=csv.DictWriter(status_output, fieldnames=['pid','isbn','large','small'])
+        for row in reader:
+            if row['status']=='True':
+                large, small=status=parser.download_img(row['pid'], row['isbn'])
+                writer.writerow({"pid":row['pid'], 'isbn':row['isbn'], "large":large, 'small':small})
     else:
         print("no match job!")
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 sudo ansible-playbook -i "localhost," -c local package.yml
 
+if [ ! -d '../html' ]; then
+    mkdir ../html
+fi
+
+if [ ! -d '../json' ]; then
+    mkdir ../json
+fi
+
+if [ ! -d '../media' ]; then
+    mkdir ../media
+    mkdir ../media/large
+    mkdir ../media/small
+fi
+
